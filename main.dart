db2bfaedcb138b1f177cf7c9f5ea4dc4de0f36fa
nikitha6798 <59058031+nikitha6798@users.noreply.github.com>
2020-08-09 21:46:20 +0530

    first commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:task1_app/ui/audio.dart';
+import 'package:task1_app/ui/video.dart';
+void main() => runApp(VideoApp());
+
+class VideoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myvideo();
+  }
+}
